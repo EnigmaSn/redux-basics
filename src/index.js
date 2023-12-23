b69f3135c89224a531bcf7bcc5daa81522032129
Anna Varaksina <AVaraksina@mlg.ru>
2023-12-23 22:05:15 +0300

    actions with params

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ const reducer = (state = 0, action) => {
       return state + 1;
     case "DEC":
       return state - 1;
+    case "RND":
+      return state + action.payload;
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
+
+document.querySelector("#rnd").addEventListener("click", (e) => {
+  const payload = Math.floor(Math.random() * 10);
+  store.dispatch({ type: "RND", payload });
+});
 
 document.querySelector("#dec").addEventListener("click", (e) => {
   store.dispatch({ type: "DEC" });
