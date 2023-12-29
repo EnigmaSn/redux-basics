d3f1348b026d88bdcd3e7dc7a168edfe896dddb3
Anna Varaksina <enigmasnape@gmail.com>
2023-12-29 11:48:47 +0300

    margin-fix

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,10 +6,10 @@ export default function Counter({ counter, inc, dec, rnd }) {
     <div className="jumbotron">
       <h2 id="counter">{counter}</h2>
       <div className="btn-group">
-        <button onClick={dec} id="dec" className="btn btn-danger btn-lg mr-10">
+        <button onClick={dec} id="dec" className="btn btn-danger btn-lg mr-1">
           DEC
         </button>
-        <button onClick={inc} id="inc" className="btn btn-success btn-lg">
+        <button onClick={inc} id="inc" className="btn btn-success btn-lg mr-1">
           INC
         </button>
         <button onClick={rnd} id="rnd" className="btn btn-primary btn-lg">
