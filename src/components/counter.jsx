42f52b21ce57c799b1155a704fbb7c6095932226
Anna Varaksina <enigmasnape@gmail.com>
2023-12-29 13:29:12 +0300

    mapDispatchToProps

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./counter.css";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../actions";
 
 const mapStateToProps = (state) => {
   return { counter: state };
+};
+const mapDispatchToProps = (dispatch) => {
+  const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+  return {
+    inc,
+    dec,
+    rnd: () => {
+      const randomValue = Math.floor(Math.random() * 10);
+      rnd(randomValue);
+    },
+  };
 };
 
 function Counter({ counter, inc, dec, rnd }) {
@@ -25,4 +39,4 @@ function Counter({ counter, inc, dec, rnd }) {
   );
 }
 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
